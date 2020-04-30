e9bf9b66090ff41e85f255a448f539052b2eda6e
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-30 20:29:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,7 +8,7 @@ class Artist
   
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-  include Param
+  include Paramble::InstanceMethods
   
   attr_accessor :name
   attr_reader :songs
