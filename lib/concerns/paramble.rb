b36c40b661e9df0e3cef976d72eec4b09acd34f7
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-30 20:28:35 +0000

    Automatically backed up by Learn

--- lib/concerns/paramble.rb
+++ lib/concerns/paramble.rb
@@ -1,7 +1,8 @@
-module Param 
+module Paramble
+  module InstanceMethods
   
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+    def to_param
+      name.downcase.gsub(' ', '-')
+    end
 
 end 
